3c542f9ddda2091b0845e9982bb86c85f1c164d7
Arthur Knoepflin <arthur.knoepflin@epitech.eu>
2018-10-14 00:59:01 +0200

    [+] WS Dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,38 +5,59 @@ import './Dashboard.css';
 export default class Dashboard extends Component {
     constructor(props) {
         super(props);
-        this.templates = [
-            {
-                id: 1,
-                name: "Taxe d'habitation",
-                description: "La taxe d'habition est un document annuel à remplir pour les impôts"
-            },
-            {
-                id: 2,
-                name: "Inscription à la crèche",
-                description: "Ce document vous permet d'inscrire votre enfant à la crèche"
-            },
-            {
-                id: 3,
-                name: "Inscription parapente",
-                description: "Ce document vous permet de vous inscrire au club de parapente de Lathuile"
-            },
-            {
-                id: 4,
-                name: "Bourse",
-                description: "Ce document vous permet d'obtenir une bourse"
-            },
-            {
-                id: 5,
-                name: "Feuille de RSA",
-                description: "Ce document permet de remplir votre fiche de RSA"
-            },
-            {
-                id: 6,
-                name: "Demande de Visa USA",
-                description: "Ce document permet d'optenir un Visa americain"
-            }
-        ];
+        this.state = {
+            templates: []
+        };
+        // this.templates = [
+        //     {
+        //         id: 1,
+        //         name: "Taxe d'habitation",
+        //         description: "La taxe d'habition est un document annuel à remplir pour les impôts"
+        //     },
+        //     {
+        //         id: 2,
+        //         name: "Inscription à la crèche",
+        //         description: "Ce document vous permet d'inscrire votre enfant à la crèche"
+        //     },
+        //     {
+        //         id: 3,
+        //         name: "Inscription parapente",
+        //         description: "Ce document vous permet de vous inscrire au club de parapente de Lathuile"
+        //     },
+        //     {
+        //         id: 4,
+        //         name: "Bourse",
+        //         description: "Ce document vous permet d'obtenir une bourse"
+        //     },
+        //     {
+        //         id: 5,
+        //         name: "Feuille de RSA",
+        //         description: "Ce document permet de remplir votre fiche de RSA"
+        //     },
+        //     {
+        //         id: 6,
+        //         name: "Demande de Visa USA",
+        //         description: "Ce document permet d'optenir un Visa americain"
+        //     }
+        // ];
+    }
+
+    _handleFetchResponse = json => {
+        console.log(json);
+    };
+
+    componentDidMount() {
+        fetch('https://reyah-hic2018.scalingo.io/models', {
+            method: 'GET'
+        })
+            .then(resp => {
+                if (resp.status >= 400)
+                    return console.error('Invalid response code');
+                return resp.json();
+            })
+            .then(json => this._handleFetchResponse(json))
+            .catch(err => console.error(err))
+
     }
 
     render() {
@@ -48,7 +69,7 @@ export default class Dashboard extends Component {
                 </div>
                 <div className={"ModelThumbList"}>
                     {
-                        this.templates.map((elem, idx) => (
+                        this.state.templates.map((elem, idx) => (
                             <ModelThumb key={idx} elemId={elem.id} name={elem.name} description={elem.description}/>
                         ))
                     }
