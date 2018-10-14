d72b8f05515e3b0bb70378a0cb60316c08f36688
Arthur Knoepflin <arthur.knoepflin@epitech.eu>
2018-10-14 15:26:07 +0200

    [~] put and get image

--- src/components/CompleteDocument.js
+++ src/components/CompleteDocument.js
@@ -13,6 +13,8 @@ export default class CompleteDocument extends Component {
             },
             fetched: false,
             input: '',
+            isFinished: false,
+            finalImgUrl: ''
         };
     }
 
@@ -38,14 +40,55 @@ export default class CompleteDocument extends Component {
             .catch(err => console.error(err))
     }
 
+
+    getImageUrl() {
+        fetch("https://reyah-hic2018.scalingo.io/generate/" + this.props.match.params.id, {
+            method: 'GET'
+        })
+            .then(resp => {
+                if (resp.status >= 400)
+                    return console.error('Invalid response code');
+                return resp.json();
+            })
+            .then(json => {
+                console.log("JSON", json);
+                if (json)
+                    this.setState({fetched: true, finalImgUrl: json.message});
+            })
+            .catch(err => console.error(err))
+    }
+
+    sendEndData = (data) => {
+        this.setState({fetched: false, isFinished: true});
+        fetch('https://reyah-hic2018.scalingo.io/models/' + this.props.match.params.id, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({data: {fields: data}})
+        })
+            .then((resp) => {
+                if (resp.status >= 400)
+                    return console.log("Error bad response code");
+                return resp.json();
+            })
+            .then(json => {
+                this.getImageUrl();
+            })
+            .catch(err => console.error(err))
+    };
+
     clickNext = () => {
         let templates = this.state.templates;
         templates.fields[this.state.stepN].value = this.state.input;
         this.setState({templates});
         if (this.isLastStep()) {
-            this.props.history.push('/')
+            this.sendEndData(this.state.templates.fields);
         } else {
-            this.setState({stepN: this.state.stepN + 1, input: this.state.templates.fields[this.state.stepN + 1].value || ''})
+            this.setState({
+                stepN: this.state.stepN + 1,
+                input: this.state.templates.fields[this.state.stepN + 1].value || ''
+            })
         }
     };
 
@@ -67,7 +110,9 @@ export default class CompleteDocument extends Component {
     getPrettyTitle = () => {
         return (
             <h1 className={"ModelTitle"}>
-                Nous devons vérifier ensemble <strong>{this.state.templates.fields.length}</strong> {'information' + (this.state.templates.fields.length > 1 ? 's' : '')} sur <i className={"ModelNameTitle"}>{this.state.templates.name}</i>
+                Nous devons vérifier
+                ensemble <strong>{this.state.templates.fields.length}</strong> {'information' + (this.state.templates.fields.length > 1 ? 's' : '')} sur <i
+                className={"ModelNameTitle"}>{this.state.templates.name}</i>
             </h1>
         )
     };
@@ -75,42 +120,59 @@ export default class CompleteDocument extends Component {
     render() {
         let buttonName = this.isLastStep() ? 'Terminé' : 'Suivant';
         return (
-            <div>
-                {(() => {
-                    if (this.state.fetched) {
-                        let currentField = this.state.templates.fields[this.state.stepN];
-                        console.log(currentField);
-                        return (
-                            <div>
-                                {this.getPrettyTitle()}
-                                <div className="questions">
-                                    <h3 className={"QuestionTitle"}>
-                                        Veuillez compléter le champs suivant
-                                    </h3>
-                                    <div className={"QuestionField"}>
-                                        <Row>
-                                            <Col s={3}>
-                                                <h4 className={"QuestionFieldName"}>
-                                                    {currentField.name}
-                                                </h4>
-                                            </Col>
-                                            <Col s={9}>
-                                                <input className={"QuestionFieldInput"} onKeyPress={this.inputKeyPress} onChange={this.inputChange} value={this.state.input}/>
-                                            </Col>
-                                        </Row>
+            (!this.state.isFinished) ?
+                <div>
+                    {(() => {
+                        if (this.state.fetched) {
+                            let currentField = this.state.templates.fields[this.state.stepN];
+                            return (
+                                <div>
+                                    {this.getPrettyTitle()}
+                                    <div className="questions">
+                                        <h3 className={"QuestionTitle"}>
+                                            Veuillez compléter le champs suivant
+                                        </h3>
+                                        <div className={"QuestionField"}>
+                                            <Row>
+                                                <Col s={3}>
+                                                    <h4 className={"QuestionFieldName"}>
+                                                        {currentField.name}
+                                                    </h4>
+                                                </Col>
+                                                <Col s={9}>
+                                                    <input
+                                                        className={"QuestionFieldInput"}
+                                                        onKeyPress={this.inputKeyPress}
+                                                        onChange={this.inputChange}
+                                                        value={this.state.input}/>
+                                                </Col>
+                                            </Row>
+                                        </div>
                                     </div>
+                                    <Button onClick={this.clickNext}
+                                            className={"waves-effect waves-block waves-light"}>{buttonName}</Button>
                                 </div>
-                                <Button onClick={this.clickNext} className={"waves-effect waves-block waves-light"}>{buttonName}</Button>
-                            </div>
-                        );
-                    } else
-                        return (
-                            <div className={'ModelThumbSpinner'}>
-                                <Preloader/>
-                            </div>
-                        )
-                })()}
-            </div>
+                            );
+                        } else
+                            return (
+                                <div className={'ModelThumbSpinner'}>
+                                    <Preloader/>
+                                </div>
+                            )
+                    })()}
+                </div>
+                :
+                <div>
+                    {
+                        (this.state.fetched) ?
+                            <Preloader/>
+                            :
+                            console.log(this.state.finalImgUrl)
+
+                    }
+                </div>
+
         )
     }
 }
+                            {/*<img src={this.state.finalImgUrl} alt={"Final document"}/>*/}
